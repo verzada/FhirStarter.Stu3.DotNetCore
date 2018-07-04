26d6341080e7bafca9d03e91571963e6ef34df85
hha046 <harald.hanssen@hnikt.no>
2018-07-04 10:20:28 +0200

    Looking at how to create a default depency inject file which is configurable

---
+++ src/FhirStarter.Bonfire.STU3.DotNetCore/Configuration/FhirStarterDefaultConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace FhirStarter.Bonfire.STU3.DotNetCore.Configuration
+{
+   public class FhirStarterDefaultConfiguration: IConfigurationSource
+    {
+        public IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/FhirStarter.Bonfire.STU3/Configuration/DefaultConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FhirStarter.Bonfire.STU3.Configuration
+{
+    class DefaultConfiguration: ConfigurationProvider
+    {
+    }
+}
